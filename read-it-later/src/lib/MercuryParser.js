41ae9a53fb082dfd3ca12cbcdb369a9648e254c1
Achraf Hadari <achrafhadari0@gmail.com>
2025-04-06 20:09:47 +0100

    lack of money, removed pupeteer

--- read-it-later/src/lib/MercuryParser.js
+++ read-it-later/src/lib/MercuryParser.js
@@ -1,5 +1,4 @@
 import Mercury from "@postlight/mercury-parser";
-import { chromium } from "playwright";
 
 const countWords = (text) => {
   return text
@@ -9,36 +8,29 @@ const countWords = (text) => {
         .filter((word) => word.length > 0).length
     : 0;
 };
+
 export async function parseArticle(url) {
   if (!url) {
     throw new Error("URL is required");
   }
 
   try {
-    //  Mercury to parse the URL and get the basic metadata
+    // Mercury to parse the URL and get the basic metadata
     const mercuryResult = await Mercury.parse(url);
     console.log("Mercury Result:", mercuryResult);
 
-    const puppeteerResult = await scrapeWithPlaywright(url);
-
-    const puppeteerContent = puppeteerResult?.content || "";
+    // Use Mercury content directly
     const mercuryContent = mercuryResult?.content || "";
 
-    const finalContent =
-      countWords(puppeteerContent) < 40 ? mercuryContent : puppeteerContent;
-
     const combinedResult = {
-      title: mercuryResult.title || puppeteerResult.title,
-      lead_image_url:
-        mercuryResult.lead_image_url ||
-        puppeteerResult.lead_image_url ||
-        "/not-found.png",
-      content: finalContent,
+      title: mercuryResult.title,
+      lead_image_url: mercuryResult.lead_image_url || "/not-found.png",
+      content: mercuryContent,
       date_published: mercuryResult.date_published,
       url: mercuryResult.url,
       domain: mercuryResult.domain,
       excerpt: mercuryResult.excerpt || "",
-      word_count: countWords(finalContent),
+      word_count: countWords(mercuryContent),
       author: mercuryResult.author,
     };
 
@@ -47,31 +39,4 @@ export async function parseArticle(url) {
     console.error("Error during article parsing:", error);
     throw error;
   }
-}
-
-// Function to scrape additional content using Puppeteer
-async function scrapeWithPlaywright(url) {
-  const browser = await chromium.launch(); // Launch Playwright browser
-  const page = await browser.newPage();
-  await page.goto(url, { timeout: 0 });
-
-  const data = await page.evaluate(() => {
-    const articleElement = document.querySelector("article");
-    if (!articleElement) {
-      return {};
-    }
-
-    const title = document.querySelector("h1")
-      ? document.querySelector("h1").innerText
-      : "";
-    const lead_image_url = document.querySelector("img")
-      ? document.querySelector("img").src
-      : "";
-    const content = articleElement.innerHTML; // Use the entire article HTML content
-
-    return { title, lead_image_url, content };
-  });
-
-  await browser.close();
-  return data;
 }
